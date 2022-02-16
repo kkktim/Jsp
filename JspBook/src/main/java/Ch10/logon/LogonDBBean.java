599c4d868396da78162a76bdbd5d8f3cfdacdf07
unknown <rkdxogh1987@gmail.com>
2022-02-16 14:30:52 +0900

    2022/02/16 교재예제

---
+++ JspBook/src/main/java/Ch10/logon/LogonDBBean.java
@@ -0,0 +1,39 @@
+package Ch10.logon;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
+public class LogonDBBean {
+	private static LogonDBBean instance = new LogonDBBean();
+	
+	public static LogonDBBean getinstance() {
+		return instance;
+		
+	}
+	
+	private LogonDBBean() {}
+	
+	private Connection getConnection() throws Exception {
+		Context initCtx = new InitialContext();
+		Context envCtx = (Context) initCtx.lookup("java:comp/env");
+		DataSource ds = (DataSource) envCtx.lookup("jdbc/jsptest");
+		return ds.getConnection();
+		
+	}
+	
+//	public int userCheck(String id, String passwd) {
+//		Connection conn = null;
+//		PreparedStatement pstmt = null;
+//		ResultSet rs = null;
+//		int x = -1;
+//		
+//		
+//	}
+}
