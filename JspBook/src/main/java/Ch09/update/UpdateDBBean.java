3b8c64a22870294850c9186b05e4426dbff89cb2
unknown <rkdxogh1987@gmail.com>
2022-02-22 18:17:02 +0900

    2022/02/22 교재예제

---
+++ JspBook/src/main/java/Ch09/update/UpdateDBBean.java
@@ -0,0 +1,13 @@
+package Ch09.update;
+
+import java.sql.Connection;
+
+public class UpdateDBBean {
+	private static UpdateDBBean instance = new UpdateDBBean();
+	
+	public UpdateDBBean() {}
+	
+//	private Connection getConnection() {
+//		
+//	}
+}
